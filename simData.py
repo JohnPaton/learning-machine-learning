3801c6bdcf731218d3fba4e884594e5b7a4f3f05
JohnPaton <john@johnpaton.net>
2016-06-20 22:11:01 -0600

    Added seperate data simulator

---
+++ simData.py
@@ -0,0 +1,28 @@
+import numpy as np
+import random
+
+# simulate N data points using polynomial coefficients coefs with domain lsit xrange
+# coefs[i] is coefficient of x**i
+# x points regularly spaced if xrand is False
+# returns np arrays x,y, sorted in increasing x order
+def simData(coefs, N, xrange=[-1,1], xrand = True):
+
+    lower = xrange[0]
+    upper = xrange[1]
+
+    if not xrand:
+        xs = np.linspace(lower,upper,N)
+    else:
+        xs = np.array([random.uniform(lower,upper) for _ in range(0,N)])
+        xs = np.sort(xs)
+
+    # np uses reverse coefficient order
+    ys = np.polyval(list(reversed(coefs)),xs)
+
+    # multiplicative noise
+    ys = np.array([random.gauss(1, 0.1)*y for y in ys])
+
+    # additive noise
+    ys = np.array([random.gauss(0, 0.1*(max(ys) - min(ys))) + y for y in ys]);
+
+    return xs,ys
