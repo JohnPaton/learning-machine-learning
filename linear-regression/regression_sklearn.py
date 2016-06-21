ea1567eebd2e6ebb6a2bfada01ebfa8a65a2fe72
JohnPaton <john@johnpaton.net>
2016-06-20 22:32:52 -0600

    initial linear regression work

---
+++ linear-regression/regression_sklearn.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import quandl, datetime, pickle
+import os
+import math
+from sklearn import preprocessing, cross_validation
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
+if os.path.isfile('WIKI_GOOGL.pickle'): # don't use all our free gets
+    df = pd.read_pickle('WIKI_GOOGL.pickle')
+else:
+    df = quandl.get('WIKI/GOOGL')
+    df.to_pickle('WIKI_GOOGL.pickle')
+
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low'])/df['Adj. Low'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']] # Just keep useful features
+
+forecast_col = 'Adj. Close'
+df = df.fillna(-99999)
+
+# predice 1% of the length of the data into the future
+forecast_out = math.ceil(0.01*len(df))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:] # most recent features have no label
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+# bootstrap data
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size = 0.2)
+
+clf = LinearRegression(n_jobs = -1)
+
+clf.fit(X_train,y_train)
+with open('linearclf.pickle','wb') as f:
+    pickle.dump(clf,f)
+
+pickle_in = open('linearclf.pickle','rb')
+clf = pickle.load(pickle_in)
+
+accuracy = clf.score(X_test,y_test)
+
+forecast_set = clf.predict(X_lately)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+ [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
+
+#print(accuracy)
+
+
+
+
+
+
